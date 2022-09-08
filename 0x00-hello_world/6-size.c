b224478633a2303bf4f7dd852177eae274144883
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-08 13:19:58 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("%d %d %d %d %d", a, b, c, d, f);
+	return (0);
+}
