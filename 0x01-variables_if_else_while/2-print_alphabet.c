c7b883d8c2f22152722a35e2b44dc281edda9f2b
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-09 14:06:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
