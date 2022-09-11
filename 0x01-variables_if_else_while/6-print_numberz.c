69c34cd160862ceefb487e2cb622de23ad07c094
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-11 10:59:28 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - Prints single digits
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
