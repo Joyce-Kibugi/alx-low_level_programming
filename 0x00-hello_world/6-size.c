2a386cff0b7537193a741b37724a15db9d3b3449
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-11 09:29:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("%d %d %d %d %d", a, b, c, d, f);
-	return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);	
 }
