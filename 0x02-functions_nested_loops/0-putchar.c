51664b4faaefb98cf31a5503a75b7c1d78616c82
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-13 05:53:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Print _putchar
+ *
+ * Return: Always (Success);
+ */
 int main(void)
 {
-
 	{
 		printf("_putchar\n");
 	}
 
-	return (0);
+        return (0);
 }
