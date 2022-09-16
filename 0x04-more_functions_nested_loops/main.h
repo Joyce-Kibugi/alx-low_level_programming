6f195c049a0026dba60f55540962f88128ac7b0d
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-16 05:31:44 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,5 +20,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int main(void);
+void print_number(void);
 
 #endif /* MAIN_H */
