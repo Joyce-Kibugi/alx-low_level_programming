e690c87f69dc139e52aa948ad9304262a73ea51c
Joyce-Kibugi <wambuikibugi@gmail.com>
2022-09-16 04:39:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *print_numbers - function that prints all numbers from
 *0 to 9
 *
-*Return: 0
+*Return: returns nothing
 */
 void print_numbers(void)
 {
